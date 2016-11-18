1d8bec013a6716d0b32c7caf0d42b719a2924348
Tom Kliethermes <kliethermes.tom@gmail.com>
2016-11-18 09:58:27 -0600

    fixed copy and paste error in license header

--- app/src/main/java/com/quaap/audiometer/MicLevelReader.java
+++ app/src/main/java/com/quaap/audiometer/MicLevelReader.java
@@ -9,12 +9,12 @@ import android.media.MediaRecorder;
  *
  *   This file is part of AudioMeter.
  *
- *   GoalTender is free software: you can redistribute it and/or modify
+ *   AudioMeter is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
  *
- *   GoalTender is distributed in the hope that it will be useful,
+ *   AudioMeter is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
