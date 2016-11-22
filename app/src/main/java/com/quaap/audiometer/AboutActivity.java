987772d32dfd32beb3819cf05854b1876c866ce3
Tom Kliethermes <kliethermes.tom@gmail.com>
2016-11-22 08:10:35 -0600

    "About" page and links

--- app/src/main/java/com/quaap/audiometer/AboutActivity.java
+++ app/src/main/java/com/quaap/audiometer/AboutActivity.java
@@ -1,7 +1,12 @@
 package com.quaap.audiometer;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            String version = pInfo.versionName;
+            int verCode = pInfo.versionCode;
+
+            System.out.println(getResources().getString(R.string.app_name));
+
+            TextView txtversion = (TextView)findViewById(R.id.txtversion);
+            txtversion.setText(version);
+
+
+            TextView txtnamelink = (TextView)findViewById(R.id.txtnamelink);
+            txtnamelink.setMovementMethod(LinkMovementMethod.getInstance());
+            txtnamelink.setText(Html.fromHtml("This program was written by <a href='http://quaap.com/D/programming-index'>Tom Kliethermes</a>"));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
