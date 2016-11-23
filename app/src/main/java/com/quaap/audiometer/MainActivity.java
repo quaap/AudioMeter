d0f6e7d59da288bdd0ac51023c7ed79e1f40e15d
Tom Kliethermes <kliethermes.tom@gmail.com>
2016-11-23 07:28:28 -0600

    Help screen updates: disclaimers and string resources.

--- app/src/main/java/com/quaap/audiometer/MainActivity.java
+++ app/src/main/java/com/quaap/audiometer/MainActivity.java
@@ -203,9 +203,9 @@ public class MainActivity extends AppCompatActivity implements MicLevelReader.Mi
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                    Toast.makeText(this, "Yay!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, R.string.got_microphone, Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(this, "This application will not function without access to the microphone", Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, R.string.need_microphone, Toast.LENGTH_LONG).show();
                 }
             }
         }
